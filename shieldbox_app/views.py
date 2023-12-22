c5ae2609215dca56660b7375bd8f64488788d159
bbbeton <jasiu.bloniarz@gmail.com>
2023-12-22 16:31:07 +0100

    Updated the server to send SMS

--- shieldbox_app/views.py
+++ shieldbox_app/views.py
@@ -6,7 +6,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, viewsets, generics
 
+from sim_module import initialize_module, sendSMS
+from config import Numbers, MAX_TEMPERATURE, PIN
 
+initialize_module()
 # Create your views here.
 
 @api_view(['GET', 'POST'])
@@ -42,27 +45,40 @@ def shieldbox_sensors(request, device_id, format = None):
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def shieldbox_sensor_detail(request, device_id, name, format = None):
+def shieldbox_sensor_detail(request, device_id, name, format=None):
     try:
-        device = ShieldBox.objects.get(id = device_id)
+        device = ShieldBox.objects.get(id=device_id)
     except ShieldBox.DoesNotExist:
         return Response({'error': 'Device not found'}, status=status.HTTP_404_NOT_FOUND)
     
     try:
-        sensor = TemperatureSensor.objects.get(device = device, name = name)
+        sensor = TemperatureSensor.objects.get(device=device, name=name)
     except TemperatureSensor.DoesNotExist:
-         return Response({'error': 'Sensor not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'Sensor not found'}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = TemperatureSensorSerializer(sensor)
         return Response(serializer.data)
     
     if request.method == 'PUT':
-        serializer = TemperatureSensorSerializer(sensor, data = request.data)
+        serializer = TemperatureSensorSerializer(sensor, data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            updated_sensor = serializer.save()
+            
+            # Check for temperature and smoke conditions
+            if updated_sensor.value >= MAX_TEMPERATURE:
+                message = f"Max shieldbox {device_id} temperature exceeded!"
+                sendSMS(Numbers, message)
+            elif updated_sensor.smoke_value:
+                message = f"Smoke detected in your shieldbox {device_id}!"
+                sendSMS(Numbers, message)
+                
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        sensor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
     if request.method == 'DELETE':
         serializer = TemperatureSensorSerializer(sensor)
